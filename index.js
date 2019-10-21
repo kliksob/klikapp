0a7a954cfb88cd33de2374d6aa6d7e93a8bd5452
Aris Riswanto <kliksob@users.noreply.github.com>
2019-10-21 09:15:04 +0700

    just update

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ app.use((req, res, next) => {
   require("./server/index.js")(req, res, next);
 });
 
-if(process.env !== 'production') {
+if (process.env !== 'production') {
   const watcher = require("chokidar").watch(["./client", "./server"]);
   watcher.on("ready", () => {
     watcher.on("all", () => {
       //console.log("Clearing App Cache...");
       Object.keys(require.cache).forEach(id => {
-        if ( (/[\/\\]client[\/\\]/.test(id)) || (/[\/\\]server[\/\\]/.test(id)) ) delete require.cache[id];
+        if ((/[\/\\]client[\/\\]/.test(id)) || (/[\/\\]server[\/\\]/.test(id))) delete require.cache[id];
       });
     });
   });
 }
 app.listen(port, () => {
-  console.log("App now is listen to PORT " + port);
+  console.log("Whoaaa server now is listening to PORT " + port);
 });
